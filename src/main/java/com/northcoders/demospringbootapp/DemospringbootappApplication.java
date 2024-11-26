0fe93f7a5a28330b1d0727ccd91dfe40a63fb873
benniwilson <benniwilson7@gmail.com>
2024-11-26 11:08:59 +0000

    feat: Added a personListController method with an appropraite endpoint which returns a list of Person objects when the endpoint recieves a get request

--- src/main/java/com/northcoders/demospringbootapp/DemospringbootappApplication.java
+++ src/main/java/com/northcoders/demospringbootapp/DemospringbootappApplication.java
@@ -1,7 +1,11 @@
 package com.northcoders.demospringbootapp;
 
+import com.northcoders.demospringbootapp.controller.DemoController;
+import com.northcoders.demospringbootapp.model.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class DemospringbootappApplication {
--- src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
+++ src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
@@ -1,8 +1,11 @@
 package com.northcoders.demospringbootapp.controller;
 
+import com.northcoders.demospringbootapp.model.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1")
@@ -12,4 +15,12 @@ public class DemoController {
     public String greeterController(){
         return "Hello there!";
     }
+
+    @GetMapping("/person")
+    public static List<Person> personListController(){
+        return List.of(new Person("Bob",34,"bob@gmail.com","Sweden","Pizza"),
+                new Person("Bill", 21, "bill@hotmail.co.uk","USA","Burger"));
+
+    }
 }
+
