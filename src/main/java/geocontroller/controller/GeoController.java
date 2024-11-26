e1d84ed45de89b23445c5919d2c2deb4c0e6d13c
Victor Boleac <victor.boleac@icloud.com>
2024-11-26 13:10:33 +0000

    refactor

--- src/main/java/geocontroller/controller/GeoController.java
+++ src/main/java/geocontroller/controller/GeoController.java
@@ -17,12 +17,13 @@ public class GeoController {
     @GetMapping("/get-coordinates")
     public String getCoordinates(@RequestParam String cityName) throws JsonProcessingException {
         CoordinatesResults testCoordinatesResults = GeocodingDAO.requestData(cityName);
-        return testCoordinatesResults.results().getFirst().toString();
+        return "Longitude: "+ testCoordinatesResults.results().getFirst().longitude()+
+                "\nLatitude: "+ testCoordinatesResults.results().getFirst().latitude();
     }
 
     @GetMapping("/get-sunrise-and-sunset")
-    public String getSunriseAndSunset(@RequestParam float lat, float lng) throws JsonProcessingException {
-        SunriseResults testCoordinatesResults = SunriseSunsetDAO.requestData(lat, lng);
+    public String getSunriseAndSunset(@RequestParam float latitude, float longitude) throws JsonProcessingException {
+        SunriseResults testCoordinatesResults = SunriseSunsetDAO.requestData(latitude, longitude);
         return testCoordinatesResults.sunriseResults().getFirst().toString();
     }
 }
